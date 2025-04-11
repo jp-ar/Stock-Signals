2957cafb3a35844d960f56c8a91d749caddfe998
jp-ar <155555709+jp-ar@users.noreply.github.com>
2025-04-11 20:12:49 -0300

    Delete ZZZTEST.py

--- ZZZTEST.py
+++
@@ -1,9 +0,0 @@
-from yahooquery import search
-
-query = "Tesla"
-resultados = search(query)
-
-for item in resultados.get("quotes", []):
-    simbolo = item.get("symbol")
-    nombre = item.get("shortname", "")
-    print(f"{simbolo} - {nombre}")
